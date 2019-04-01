01d7464e3521d27d411e80a1a81583effb7f23c3
Peter Gagliardi <ptrgags@gmail.com>
2019-04-01 17:16:12 -0400

    Add a Helix path

--- path.py
+++ path.py
@@ -1,3 +1,5 @@
+import math
+
 from mathutils import Vector
 
 class Path:
@@ -69,3 +71,102 @@ class Line(Path):
         # TODO: Handle the case when the tangent is pointing upwards or
         # downwards
         return direction
+
+class Helix(Path):
+    """
+    Helical path around the z-axis with customizable start/end heights 
+    and angles
+    """
+    def __init__(self, heights, angles):
+        """
+        heights: (z0, zf) (bottom and top heights)
+        angles: (phi0, phif) (start and stop angle in radians.)
+
+        For example, for a helix with 4 loops from -1 to 1 in the z direction
+        would have:
+
+        heights: (-1, 1)
+        angles: (0, 4 * pi)
+        """
+        self.heights = heights
+        self.angles = angles
+
+    def position(self, v):
+        """
+        Position on the helix:
+
+        x = cos(phi(v))
+        y = sin(phi(v))
+        z = z(v)
+
+        phi(v) = lerp(angles, v)
+        z(v) = lerp(heights, v)
+        """
+        # The height is a linear interpolation of the two heights
+        z0, zf = self.heights
+        z = (1.0 - v) * z0 + v * zf
+
+        # For x and y, the angle gets lerp-ed
+        phi0, phif = self.angles
+        phi = (1.0 - v) * phi0 + v * phif
+        x = math.cos(phi)
+        y = math.sin(phi)
+        return Vector((x, y, z))
+
+    def tangent(self, v):
+        """
+        The tangent can be found by taking the derivative of
+        the position:
+
+        x' = -sin(phi(v))phi'(v) = (phif - phi0) * -sin(phi(v))
+        y' = cos(phi(v))phi'(v) = (phif - phi0) * cos(phi(v))
+        z' = z'(v) = zf - z0
+
+        Then normalize the vector (x', y', z') to get the unit direction.
+        """
+        # the slope in the z direction is simply the change in height
+        z0, zf =  self.heights
+        dz = zf - z0
+
+        # Same thing for the angle
+        phi0, phif = self.angles
+        phi = (1.0 - v) * phi0 + v * phif
+        dphi = phif - phi0
+
+        # The rest is the chain rule from calculus
+        dx = -math.sin(phi) * dphi
+        dy = math.cos(phi) * dphi
+
+        T = Vector((dx, dy, dz))
+        T.normalize()
+        return T
+
+    def normal(self, v):
+        """
+        The normal direction can be found from the second derivative
+        of position
+
+        x'' = -phi'(v)^2 * cos(phi(v)) = -(phif - phi0)^2 * cos(phi(v))
+        y'' = -phi'(v)^2 * sin(phi(v)) = -(phif - phi0)^2 * sin(phi(v))
+        z'' = 0 (z' is constant)
+
+        Again, normalize to get the unit normal.
+        """
+        # The height changes linearly, so it has no vertical acceleration
+        ddz = 0
+
+        # Handle the coefficient and angle
+        phi0, phif = self.angles
+        phi = (1.0 - v) * phi0 + v * phif
+        dphi = phif - phi0
+        dphi_sqr = dphi * dphi
+
+        ddx = -dphi_sqr * math.cos(phi)
+        ddy = -dphi_sqr * math.sin(phi)
+
+        N = Vector((ddx, ddy, ddz))
+        N.normalize()
+        return N
+
+
+
